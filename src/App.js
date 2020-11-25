db7007bb58bb406f215e101505b48af31f06e144
Alejandro <alejandro.rudolphy@alumnos.uv.cl>
2020-11-25 17:20:39 -0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload pls.
+          Edit <code>src/App.js</code> and save to reload plssss.
         </p>
         <a
           className="App-link"
